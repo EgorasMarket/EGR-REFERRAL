d182b408569e63cee0f3dfec414f2c0a41e9af4c
Ebrinix <ebrinix121@yahoo.com>
2021-12-15 14:17:57 +0100

    Update dollar All Users

--- src/components/layout/Paginationd.js
+++ src/components/layout/Paginationd.js
@@ -12,52 +12,64 @@ import axios from "axios";
 import { API_URL as api_url } from "../../actions/types";
 
 const columns = [
-  { id: 'name', label: 'Username', minWidth: 170 },
-  { id: 'code', label: 'Email', minWidth: 100 },
+  { id: 'username', label: 'Username', minWidth: 170 },
+  { id: 'email', label: 'Email', minWidth: 100 },
   {
-    id: 'population',
+    id: 'is_active',
     label: 'Is Active',
     minWidth: 170,
     align: 'right',
     // format: (value) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
+    id: 'referer',
     label: 'Referer',
     minWidth: 170,
     align: 'right',
     // format: (value) => value.toLocaleString('en-US'),
   },
   {
-    id: 'density',
-    label: 'Density',
+    id: 'walletAddress',
+    label: 'Wallet Address',
     minWidth: 170,
     align: 'right',
     // format: (value) => value.toFixed(2),
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+// function createData(name, code, population, size) {
+//   const density = population / size;
+//   return { name, code, population, size, density };
+// }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+   {
+     name: 'India',
+     code: 'IN',
+     population: '12388444',
+     size: "2122"
+   },   {
+    name: 'India 2',
+    code: 'IN2',
+    population: '12388444',
+    size: "2122"
+  }
+
+  // createData('India', 'IN', 1324171354, 3287263),
+  // createData('China', 'CN', 1403500365, 9596961),
+  // createData('Italy', 'IT', 60483973, 301340),
+  // createData('United States', 'US', 327167434, 9833520),
+  // createData('Canada', 'CA', 37602103, 9984670),
+  // createData('Australia', 'AU', 25475400, 7692024),
+  // createData('Germany', 'DE', 83019200, 357578),
+  // createData('Ireland', 'IE', 4857000, 70273),
+  // createData('Mexico', 'MX', 126577691, 1972550),
+  // createData('Japan', 'JP', 126317000, 377973),
+  // createData('France', 'FR', 67022000, 640679),
+  // createData('United Kingdom', 'GB', 67545757, 242495),
+  // createData('Russia', 'RU', 146793744, 17098246),
+  // createData('Nigeria', 'NG', 200962417, 923768),
+  // createData('Brazil', 'BR', 210147125, 8515767),
 ];
 
 export default function Paginationd() {
@@ -108,7 +120,7 @@ export default function Paginationd() {
 
   return (
     <Paper style={{marginTop: '120px'}} sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 640 }}>
+      <TableContainer sx={{ maxHeight: 800 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
@@ -160,18 +172,19 @@ export default function Paginationd() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            {
+              topReferral.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} >
                     {columns.map((column) => {
                       const value = row[column.id];
+                      console.log(value);
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
+                          {value === true
+                            ? 1
+                            : value === false ? 0 : value}
                         </TableCell>
                       );
                     })}
@@ -182,9 +195,9 @@ export default function Paginationd() {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[10, 25, 50, 100]}
         component="div"
-        count={rows.length}
+        count={topReferral.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
