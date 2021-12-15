f8b9631aa6aff054e6bcd79e9fef6417e1c19eaf
Ebrinix <ebrinix121@yahoo.com>
2021-12-15 14:22:25 +0100

    Update dollar All Users

--- src/components/layout/Paginationd.js
+++ src/components/layout/Paginationd.js
@@ -37,40 +37,6 @@ const columns = [
   },
 ];
 
-// function createData(name, code, population, size) {
-//   const density = population / size;
-//   return { name, code, population, size, density };
-// }
-
-const rows = [
-   {
-     name: 'India',
-     code: 'IN',
-     population: '12388444',
-     size: "2122"
-   },   {
-    name: 'India 2',
-    code: 'IN2',
-    population: '12388444',
-    size: "2122"
-  }
-
-  // createData('India', 'IN', 1324171354, 3287263),
-  // createData('China', 'CN', 1403500365, 9596961),
-  // createData('Italy', 'IT', 60483973, 301340),
-  // createData('United States', 'US', 327167434, 9833520),
-  // createData('Canada', 'CA', 37602103, 9984670),
-  // createData('Australia', 'AU', 25475400, 7692024),
-  // createData('Germany', 'DE', 83019200, 357578),
-  // createData('Ireland', 'IE', 4857000, 70273),
-  // createData('Mexico', 'MX', 126577691, 1972550),
-  // createData('Japan', 'JP', 126317000, 377973),
-  // createData('France', 'FR', 67022000, 640679),
-  // createData('United Kingdom', 'GB', 67545757, 242495),
-  // createData('Russia', 'RU', 146793744, 17098246),
-  // createData('Nigeria', 'NG', 200962417, 923768),
-  // createData('Brazil', 'BR', 210147125, 8515767),
-];
 
 export default function Paginationd() {
   const [page, setPage] = React.useState(0);
